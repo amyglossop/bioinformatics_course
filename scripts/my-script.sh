2ea1543139aad676fb3612eee2a016ad68a73526
amyglossop <48533295+amyglossop@users.noreply.github.com>
2019-03-20 14:36:17 +0000

    Update my-script.sh

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,13 +1,9 @@
 #!/usr/bin/env bash
+# make empty directories in your git repo
 mkdir -p analysis docs data
 
-for my_directory in scripts analyis docs data;do
+# add a README.md to each directory
+for my_directory in scripts analysis docs data;do
   touch ${my_directory}/README.md
-  echo "# ${my_directory}" >> ${my_directory}/README.md
+  echo “# ${my_directory}” >> ${my_directory}/README.md
 done
-
-chmod +x my-script.sh
-
-git add .
-git commit -m "not tested: updated script to make empty dirs and readme.md"
-git push
